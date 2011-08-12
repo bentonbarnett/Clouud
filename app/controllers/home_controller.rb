f67f824639ce291348c20d664ddcc83a88a11e2f
Benton Barnett <benton@bentonbarnett.com>
2011-08-12 07:46:51 -0700

    Faked the active rooms list for demonstration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    
+    @active_rooms = active_rooms
+  end
+  
+  private
+  
+  def active_rooms
+    ['portland', 'madison', 'bonfire', 'beatles', 'sxsw room 105']
   end
 end

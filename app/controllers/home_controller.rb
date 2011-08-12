f9423ff1dd7641bdd5f0ef9e27aa9745a4550485
Benton Barnett <benton@bentonbarnett.com>
2011-08-11 22:33:45 -0700

    Remove faked near by rooms feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @nearby_rooms = nearby_rooms
-  end
-  
-  private
-  
-  def nearby_rooms
-    # Actually, this just returns recently used rooms until
-    # we get geolocation in place.
-    messages = Message.select("DISTINCT room").limit("10").order("created_at DESC")
     
-    messages.collect{|message| message.room}
   end
 end

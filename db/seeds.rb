e520f55328134514636d7eeb85b00636c9fcd053
Benton Barnett <benton@bentonbarnett.com>
2012-01-15 12:17:07 -0800

    Removed broken seed data and set up proper seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Create nickname colors
+Color.find_or_create_by_hex("EDAFC0")
+Color.find_or_create_by_hex("626E69")
+Color.find_or_create_by_hex("AD808D")
+Color.find_or_create_by_hex("909B8A")
+
